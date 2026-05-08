4750b8ef92986f48da98c3db532ea157c6d8c2d5
koray-afk <abhiman030@gmail.com>
2026-05-08 20:06:49 +0530

    Remove pricing section from home page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -5,7 +5,6 @@ import { Tracking } from "@/components/sections/tracking";
 import { Appointments } from "@/components/sections/appointments";
 import { Wellness } from "@/components/sections/wellness";
 import { Testimonials } from "@/components/sections/testimonials";
-import { Pricing } from "@/components/sections/pricing";
 import { Faq } from "@/components/sections/faq";
 import { Articles } from "@/components/sections/articles";
 import { AppShowcase } from "@/components/sections/app-showcase";
@@ -22,7 +21,6 @@ export default function Home() {
       <AppShowcase />
       <Wellness />
       <Testimonials />
-      <Pricing />
       <Faq />
       <Articles />
       <Newsletter />
